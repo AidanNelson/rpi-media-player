e5c18557ecb8b90a58c42533b21bc435c84dd159
AidanNelson <AidanNelson@users.noreply.github.com>
2022-06-14 14:22:26 -0400

    add network interface

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const network = require('network-config');
 
 const io = new Server();
 const clients = {};
@@ -16,3 +17,20 @@ setTimeout(() => {
   console.log("Telling clients to play video!");
   io.sockets.emit('play');
 },10000);
+
+
+network.interfaces(function(err, interfaces){
+  /* interfaces should be something like:
+
+  [{
+    name: 'eth0',
+    ip: '1.1.1.77',
+    netmask: '1.1.1.0',
+    mac: 'aa:aa:aa:aa:aa:aa',
+    gateway: '10.10.10.1', 
+   },
+   { ... }, { ... }]
+  */
+ console.log(interfaces);
+ 
+});
