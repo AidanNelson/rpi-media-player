aaf5153e256858b67189290a523bccdd45a74cbc
AidanNelson <AidanNelson@users.noreply.github.com>
2022-06-14 14:55:44 -0400

    remove networking

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const { Server } = require("socket.io");
-const network = require("network-config");
+// const network = require("network-config");
 
 const io = new Server();
 const clients = {};
@@ -17,34 +17,34 @@ setTimeout(() => {
   io.sockets.emit("play");
 }, 10000);
 
-network.interfaces(function (err, interfaces) {
-  /* interfaces should be something like:
+// network.interfaces(function (err, interfaces) {
+//   /* interfaces should be something like:
 
-  [{
-    name: 'eth0',
-    ip: '1.1.1.77',
-    netmask: '1.1.1.0',
-    mac: 'aa:aa:aa:aa:aa:aa',
-    gateway: '10.10.10.1', 
-   },
-   { ... }, { ... }]
-  */
-  console.log(interfaces);
-}, {interfaces: {parse: true, file: '/etc/dhcpcd.conf'}});
+//   [{
+//     name: 'eth0',
+//     ip: '1.1.1.77',
+//     netmask: '1.1.1.0',
+//     mac: 'aa:aa:aa:aa:aa:aa',
+//     gateway: '10.10.10.1', 
+//    },
+//    { ... }, { ... }]
+//   */
+//   console.log(interfaces);
+// }, {interfaces: {parse: true, file: '/etc/dhcpcd.conf'}});
 
-var network_configuration = {
-  ip: "192.168.1.10",
-  netmask: "255.255.255.0",
-  gateway: "192.168.1.1",
-  restart: true,
-};
+// var network_configuration = {
+//   ip: "192.168.1.10",
+//   netmask: "255.255.255.0",
+//   gateway: "192.168.1.1",
+//   restart: true,
+// };
 
-network.configure("eth0", network_configuration, function (err) {
-  console.log(err);
-});
+// network.configure("eth0", network_configuration, function (err) {
+//   console.log(err);
+// });
 
-setTimeout(() => {
-  network.interfaces(function (err, interfaces) {
-    console.log(interfaces);
-  });
-}, 5000);
+// setTimeout(() => {
+//   network.interfaces(function (err, interfaces) {
+//     console.log(interfaces);
+//   });
+// }, 5000);
