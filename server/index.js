139e27a97c0913cf7fb7fee68693a4d6bdcd125f
AidanNelson <AidanNelson@users.noreply.github.com>
2022-12-04 16:38:43 -0500

    add stub admin console and HTTPS / Express to server

--- server/index.js
+++ server/index.js
@@ -1,25 +1,61 @@
-const { Server } = require("socket.io");
+const express = require("express");
+const https = require("https");
+const devcert = require("devcert");
+const fs = require("fs");
 
-const io = new Server();
-const clients = {};
+async function main() {
+  // Set up express to serve the admin console
+  const app = express();
+  console.log("Serving static files at ", process.cwd() + "/admin");
+  app.use(express.static(process.cwd() + "/admin"));
 
-io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
-  clients[socket.id] = {};
-});
+  // set up HTTPS server and SSL certificates
+  let ssl;
+  if (process.env.ENVIRONMENT === "PRODUCTION") {
+    ssl = {
+      key: fs.readFileSync("/path/to/private.key"),
+      cert: fs.readFileSync("/path/to/your_domain_name.crt"),
+    };
+  } else {
+    ssl = await devcert.certificateFor("localhost");
+  }
+  const server = https.createServer(ssl, app);
 
-io.listen(3000);
-console.log("Server listening on port 3000");
+  // have our HTTPS server listen on HTTPS standard port (443)
+  const port = 443;
+  server.listen(port);
+  console.log(`Server listening on port ${port}`);
 
-function restartVideo() {
-  console.log("Telling clients to stop video!");
-  io.sockets.emit("stop");
+  // set up our socket.io server using the HTTPS server (with permissive CORS)
+  let io = require("socket.io")();
+  io.listen(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  // keep track of our clients for any admin purposes...
+  const clients = {};
+
+  io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+    clients[socket.id] = {};
+  });
+
+  function restartVideo() {
+    console.log("Telling clients to stop video!");
+    io.sockets.emit("stop");
+    setTimeout(() => {
+      console.log("Telling clients to play video!");
+      io.sockets.emit("play");
+    }, 5000);
+  }
+
   setTimeout(() => {
-    console.log("Telling clients to play video!");
-    io.sockets.emit("play");
-  }, 5000);
+    restartVideo();
+  }, 10000);
 }
 
-setTimeout(() => {
-  restartVideo();
-}, 10000);
+main();
